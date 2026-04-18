b02793ba2091c24562037e07a0140ced604709db
Marcus Lee <mailingmarcuslee@gmail.com>
2026-04-17 22:17:27 -0700

    Update mod.rs

--- src/app/home/mod.rs
+++ src/app/home/mod.rs
@@ -6,13 +6,13 @@ pub use storage::{RecentItem, RecentStore};
 pub(super) use path::{normalize_database_path, recent_path_is_available, recent_path_label};
 #[cfg(test)]
 pub(super) use path::{path_to_sqlite_uri_path, recent_paths_match};
+#[cfg(all(test, unix))]
+pub(super) use storage::path_from_storage_bytes;
+#[cfg(test)]
+pub(super) use storage::path_to_storage_bytes;
 pub(super) use storage::{
     AppStorage, StoredFilterRule, StoredSession, StoredSortRule, StoredTableState,
 };
-#[cfg(test)]
-pub(super) use storage::path_to_storage_bytes;
-#[cfg(all(test, unix))]
-pub(super) use storage::path_from_storage_bytes;
 
 #[cfg(test)]
 mod tests;
